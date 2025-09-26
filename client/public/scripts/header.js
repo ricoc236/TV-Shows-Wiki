2fed83fd2ece01ef75b11a42fbbdd765741613aa
Rico C <ricochen23166@gmail.com>
2025-09-26 02:01:40 -0400

    smooth scroll fix on main page

--- client/public/scripts/header.js
+++ client/public/scripts/header.js
@@ -6,11 +6,17 @@ headerLogo.className = 'logo';
 headerLogo.style.height = '6rem';
 const headerTitle = document.createElement('h1');
 headerTitle.textContent = 'TV Shows Wiki';
-const allShowsButton = document.createElement('a');
+
+const allShowsButton = document.createElement('button'); 
 allShowsButton.textContent = 'All Shows';
-allShowsButton.href = '/#main-content'; 
-allShowsButton.setAttribute('role', 'button'); 
 allShowsButton.className = 'contrast all-shows-button';
+allShowsButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const mainContent = document.getElementById('main-content');
+  if (mainContent) {
+    mainContent.scrollIntoView({ behavior: 'smooth' });
+  }
+});
 header.appendChild(headerLogo);
 header.appendChild(headerTitle);
 header.appendChild(allShowsButton);
